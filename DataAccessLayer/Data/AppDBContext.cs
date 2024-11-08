8742e3703036d24b7f279c812e94cea26ef50847
Fatima <fatimavaliyeva0704@gmail.com>
2024-11-09 00:19:35 +0100

    Add project files.

---
+++ DataAccessLayer/Data/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCMiniProject.DataAccessLayer
+{
+    public class AppDBContext : IdentityDbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+
+
+
+
+    }
+}
